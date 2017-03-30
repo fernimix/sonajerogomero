dbe23ba38bacc60495d04d5b85e2ce7719212a5a
Fernando López León <fernandolopezleon@gmail.com>
2017-03-30 22:39:56 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function() {
+  //$('#pagepiling').pagepiling();
+
+  $('#pagepiling').pagepiling({
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    menu: '#myMenu'
+  });
+});
